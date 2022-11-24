af2f511158bd2567c671f9465810499a3dcae74a
Jack Potier <jackpotierschool@gmail.com>
2022-11-25 10:46:14 +1300

    console.log id of the element clicked

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let number1 = 1;
-let number2 = 2;
-let ans = null;
+//Set defaults
+let buttonGrid = document.querySelectorAll(".grid-item");
+buttonArray = Array.from(buttonGrid);
 
-function addition(){
-    ans = number1 + number2;
-    console.log(ans);
+
+//Identifies the button pressed and gets its value
+buttonArray.forEach(function(getValue){
+    getValue.addEventListener("click", function() {
+        console.log(getValue.id);
+    })
+});
+
+
+
+//Makes a click sound on button press
+function calClick(){
+    const click = new Audio("click.wav");
+    buttonGrid.forEach(function(playSound) {
+        playSound.addEventListener("click", function() {
+            click.play();
+        });
+    });
 }
 
-addition()
