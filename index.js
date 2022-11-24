bcc47d0bae1260b68055e08930982aa0cec71dbe
Jack Potier <jackpotierschool@gmail.com>
2022-11-25 11:13:34 +1300

    shooting errors in //turn number classes

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 //Set defaults
+const click = new Audio("click.wav");
 let buttonGrid = document.querySelectorAll(".grid-item");
 buttonArray = Array.from(buttonGrid);
 
-
-//Identifies the button pressed and gets its value
-buttonArray.forEach(function(getValue){
-    getValue.addEventListener("click", function() {
-        console.log(getValue.id);
-    })
-});
-
-
+//Turn number classes into operable numbers
+let numClassArray = document.querySelectorAll(".number");
+let numbers = [];
+for (let i=1; i<=10; i++){
+    numbers[i] = numClassArray[i].class;
+}
 
 //Makes a click sound on button press
 function calClick(){
-    const click = new Audio("click.wav");
     buttonGrid.forEach(function(playSound) {
         playSound.addEventListener("click", function() {
             click.play();
@@ -22,3 +19,14 @@ function calClick(){
     });
 }
 
+
+//Identifies the button pressed and gets its value
+buttonGrid.forEach(function(getValue){
+    getValue.addEventListener("click", function() {
+        calClick();
+        console.log(numbers);
+    })
+});
+
+
+//Display 
