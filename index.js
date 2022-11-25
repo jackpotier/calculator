314f0815dcf45dde14148353a4d535cd0235257c
Jack Potier <jackpotierschool@gmail.com>
2022-11-25 18:01:53 +1300

    deleted stupid, disfunctional code

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const click = new Audio("click.wav");
 let buttonGrid = document.querySelectorAll(".grid-item");
 buttonArray = Array.from(buttonGrid);
 
-//Turn number 'strings' into operable numbers
-let numberStringGrid = document.getElementsByClassName("number")
-let numberStringArray = Array.from(numberStringGrid);
-for (let i=0; i<10; i++){
-    numberStringArray[i].substring('button#'.length)
-}
-numberStringArray.sort(function(a, b){return a - b});
-console.log(numberStringArray);
 
 //Makes a click sound on button press
 function calClick(){
@@ -26,7 +18,6 @@ function calClick(){
 buttonGrid.forEach(function(getValue){
     getValue.addEventListener("click", function() {
         calClick();
-        console.log(numbers);
     })
 });
 
