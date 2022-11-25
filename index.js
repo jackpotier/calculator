80a2ad82a871399a95d704e838d26a062ec079e1
Jack Potier <jackpotierschool@gmail.com>
2022-11-25 19:27:10 +1300

    displays memory array in currentMemory

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const click = new Audio("click.wav");
 let buttonGrid = document.querySelectorAll(".grid-item");
 buttonArray = Array.from(buttonGrid);
+let currentMemory = document.querySelector("#currentMemory");
+counter = 0;
+memoryDrive = [];
 
 
 //Makes a click sound on button press
@@ -14,12 +17,18 @@ function calClick(){
 }
 
 
+//Displays button press input on the top of display screen
+function displayMemory(getValue){
+    memoryDrive[counter] = getValue.id;
+    currentMemory.textContent = memoryDrive;
+    counter+=1
+}
+
+
 //Identifies the button pressed and gets its value
 buttonGrid.forEach(function(getValue){
     getValue.addEventListener("click", function() {
         calClick();
+        displayMemory(getValue);
     })
 });
-
-
-//Display text on current memory
